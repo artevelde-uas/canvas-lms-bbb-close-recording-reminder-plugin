3da3a66cf56516c84e3fdfae286157f5d4625283
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2020-03-31 13:25:22 +0200

    Show reminder label

--- src/index.js
+++ src/index.js
@@ -4,5 +4,17 @@ import { addDOMObserver } from './util';
 export default function ({ router }) {
     router.addListener('courses.conferences', params => {
 
+        addDOMObserver('.close_conference_link', element => {
+            let row = element.closest('.ig-row');
+
+            if (row === null) return;
+
+            row.querySelector('.ig-info > .has_actions').insertAdjacentHTML('beforeend', `
+                <span class="label label-warning ig-row-label">
+                    Don't forget to end this conference!
+                </span>
+            `);
+        });
+
     });
 }
