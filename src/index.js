3beae99c334a0f8d31acca21df32b57cb30ffa0a
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2020-03-31 13:28:13 +0200

    Add translations

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import { addDOMObserver } from './util';
 
+import translations from './i18n.json';
 
-export default function ({ router }) {
+
+export default function ({ router, i18n: { translate: __, setTranslations } }) {
     router.addListener('courses.conferences', params => {
+
+        setTranslations(translations);
 
         addDOMObserver('.close_conference_link', element => {
             let row = element.closest('.ig-row');
@@ -11,7 +15,7 @@ export default function ({ router }) {
 
             row.querySelector('.ig-info > .has_actions').insertAdjacentHTML('beforeend', `
                 <span class="label label-warning ig-row-label">
-                    Don't forget to end this conference!
+                    ${__('dont_forget_to_end_conference')}
                 </span>
             `);
         });
